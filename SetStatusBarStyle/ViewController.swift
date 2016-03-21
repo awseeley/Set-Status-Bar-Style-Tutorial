6e21fc1a68fca18aa2d7766f4d9333bb9937a974
Andrew Seeley <anseeley@au1.ibm.com>
2016-03-21 18:09:44 +1100

    Set Status Bar Style Tutorial
    Set Status Bar Style Tutorial

---
+++ SetStatusBarStyle/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SetStatusBarStyle
+//
+//  Created by Andrew on 2/03/2016.
+//  Copyright © 2016 Seemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.redColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+
+}
+
