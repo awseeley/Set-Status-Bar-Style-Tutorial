7b4b1f1a30165d5e53eec183a093442e3140eb6f
Andrew Seeley <awseeley@gmail.com>
2017-01-29 18:22:51 +1100

    Updated for Swift 3
    Updated for Swift 3

--- SetStatusBarStyle/ViewController.swift
+++ SetStatusBarStyle/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.redColor()
+        self.view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,8 +21,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return UIStatusBarStyle.LightContent
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
     }
     
 
--- SetStatusBarStyleTests/SetStatusBarStyleTests.swift
+++ SetStatusBarStyleTests/SetStatusBarStyleTests.swift
@@ -28,7 +28,7 @@ class SetStatusBarStyleTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
